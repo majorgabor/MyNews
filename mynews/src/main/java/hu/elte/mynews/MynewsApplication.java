8f5b211628c41fd231c38110443b83614545ce4e
majorgabor <majorg.96@gmail.com>
2017-10-31 23:52:44 +0100

    baseprogram

---
+++ mynews/src/main/java/hu/elte/mynews/MynewsApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.mynews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynewsApplication.class, args);
+	}
+}
