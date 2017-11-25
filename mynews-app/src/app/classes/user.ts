9fbf3583890f0bd49f14819fe1f55a6662285f87
majorgabor <majorg.96@gmail.com>
2017-11-25 16:17:49 +0100

    user class

---
+++ mynews-app/src/app/classes/user.ts
@@ -0,0 +1,18 @@
+
+export enum Role{
+    GUEST, USER, ADMIN
+}
+
+export class User {
+    public id: number;
+
+    public constructor(
+        private email: string,
+        private name: string,
+        private age: number,
+        private city: string,
+        private password: string,
+        private date: Date,
+        private role: Role
+    ) { }
+}
